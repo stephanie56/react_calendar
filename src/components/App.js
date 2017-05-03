dc4d2b2065d5ce73ef87128d58b078f5b061e948
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-05-03 00:49:33 -0400

    Moved event data to db json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
 import Calendar from './Calendar';
+const db = require('../data/events');
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      events:[
-        {
-          id: 1,
-          date: "05/21/2017",
-          startTime: "10:00",
-          endTime: "15:00",
-          title: "Stephanie's Birthday",
-          detail: "Celebrating Steph's big day with gangs in a fun karaoke night",
-        },
-        {
-          id: 2,
-          date: "03/24/2017",
-          startTime: "10:00",
-          endTime: "23:00",
-          title: "Someone Else's Birthday",
-          detail: "Heyyyy yo guess who's this birthday? shhhhh... ",
-        }
-      ],
-    }
-  };
+      events: db.events,
+    };
+  }
 
   render() {
 
---
+++ src/data/events.js
@@ -0,0 +1,20 @@
+module.exports = {
+    events:[
+      {
+        id: 1,
+        date: "05/21/2017",
+        startTime: "10:00",
+        endTime: "15:00",
+        title: "Stephanie's Birthday",
+        detail: "Celebrating Steph's big day with gangs in a fun karaoke night",
+      },
+      {
+        id: 2,
+        date: "03/24/2017",
+        startTime: "10:00",
+        endTime: "23:00",
+        title: "Someone Else's Birthday",
+        detail: "Heyyyy yo guess who's this birthday? shhhhh... ",
+      }
+    ]
+};
