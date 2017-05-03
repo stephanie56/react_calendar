2372cad86c2f4aa949050358621cbfbd22dcf974
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-05-02 23:52:25 -0400

    Added styles to DateView and EventView

--- src/components/EventTitle.js
+++ src/components/EventTitle.js
@@ -7,14 +7,13 @@ class EventTitle extends Component {
     const startHour = parseInt(startTime[0], 10);
     const endTime = this.props.endTime.split(":");
     const endHour = parseInt(endTime[0], 10);
+    const start = `${startHour > 12 ? startHour - 12 : startHour}:${ startTime[1] } ${ startHour <= 12 ? "AM" : "PM" }`;
+    const end = `${endHour > 12 ? endHour - 12 : endHour}:${ endTime[1] } ${ endHour <= 12 ? "AM" : "PM" }`;
 
     return (
       <div className="Title">
         <h2>{this.props.title}</h2>
-        <h3>{this.props.date} |
-            {startHour > 12 ? startHour - 12 : startHour}:{ startTime[1] } { startHour <= 12 ? "AM" : "PM" } - 
-            {endHour > 12 ? endHour - 12 : endHour}:{ endTime[1] } { endHour <= 12 ? "AM" : "PM" }
-        </h3>
+        <h3>{this.props.date} @ { start } - { end }</h3>
       </div>
     );
   }
